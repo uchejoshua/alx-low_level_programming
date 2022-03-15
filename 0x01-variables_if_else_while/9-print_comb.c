00f4b485a5b74a04ae799ce3cfe1eb2c26b2ae6a
uchejoshua <joshuauche02@gmail.com>
2022-03-15 21:31:20 +0000

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ ** main - prints out all the numbers under 10 with a ,
+ ** Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
