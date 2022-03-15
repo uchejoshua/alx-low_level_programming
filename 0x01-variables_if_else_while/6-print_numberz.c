9cdfcc9adf461c4170dcbc8f1807fc1fdbdb8161
uchejoshua <joshuauche02@gmail.com>
2022-03-15 21:19:40 +0000

    prints all asci single numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers of base 10
+ * Return: 0 executed successfully
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num >= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
