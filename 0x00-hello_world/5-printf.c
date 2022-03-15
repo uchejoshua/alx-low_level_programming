948b20b18f18ab48afd7b5497cfd8f6f732fa0ec
uchejoshua <joshuauche02@gmail.com>
2022-03-15 12:52:32 +0000

    C program that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string using printf function
+ * Return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+	/*prints the strings inputed*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
