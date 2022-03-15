034dc16a851a24e66410dea7351221436231277c
uchejoshua <joshuauche02@gmail.com>
2022-03-15 21:13:41 +0000

    prints all single numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers of base 10
+ * Return: 0 executed successfully
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
