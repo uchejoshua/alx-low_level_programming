5f9a5f8e294281f43753ddcacee09d98d3f8b630
uchejoshua <joshuauche02@gmail.com>
2022-03-23 07:30:45 +0000

    positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints positve or nagetive function
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
